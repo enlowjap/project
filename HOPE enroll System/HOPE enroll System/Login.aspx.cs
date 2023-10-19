ef10dd2469bb424c020feebb7607fde9224c2e5f
enlowjap <145821911+enlowjap@users.noreply.github.com>
2023-10-19 09:25:08 +0800

    database babaguhin pa

--- HOPE enroll System/HOPE enroll System/Login.aspx.cs
+++ HOPE enroll System/HOPE enroll System/Login.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace HOPE_enroll_System
 {
@@ -16,6 +18,10 @@ namespace HOPE_enroll_System
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection("Data Source=JAPHET;Initial Catalog=HOPE;Integrated Security=True");
+            con.Open();
+            SqlCommand com = new SqlCommand("Insert into Students (LastName,EmailAddress,Passwrd) values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "'); ", con);
+            con.Close();
             Response.Redirect("Verification.aspx");
         }
     }
